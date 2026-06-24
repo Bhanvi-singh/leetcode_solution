621684b93a1a689e8a3f3778d2aa00de6e236937
Bhanvi-singh <singhbhanvi99@gmail.com>
2026-06-25 00:47:33 +0530

    Time: 3 ms (55.14%), Space: 12.4 MB (17.86%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x < 1:
+           return x
+        low = 1
+        high = x
+        
+        while low <= high:
+            mid = (low + high ) // 2
+            if mid * mid == x:
+              return mid
+            elif mid * mid < x:
+              
+                 low = mid + 1
+            else:
+                high = mid - 1
+        return high
