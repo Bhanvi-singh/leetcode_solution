326469c756da507478906088cd2003ff9eea9f28
Bhanvi-singh <singhbhanvi99@gmail.com>
2026-07-06 00:43:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isNStraightHand(self, hand, groupSize):
+        """
+        :type hand: List[int]
+        :type groupSize: int
+        :rtype: bool
+        """
+        n = len(hand)
+        if n % groupSize != 0:
+            return False
+        hand.sort()
+        freq = Counter(hand)
+        for num in hand:
+            if freq[num] == 0:
+                continue
+            for curr in range(num, num + groupSize):
+                if freq[cur
+                r] == 0:
+                    return False
+                freq[curr] -= 1
+        return True
