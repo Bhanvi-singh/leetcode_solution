a801567c77d588db147825128fdfa84916c2be9d
Bhanvi Singh <singhbhanvi99@gmail.com>
2026-09-17 00:30:54 +0530

    Time: 394 ms (42.86%), Space: 12.6 MB (14.29%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.py
@@ -7,21 +7,21 @@ class Solution(object):
         """
         MOD = 10**9 + 7
 
-        # dp[j] = ways to make exactly j segments
+       
         dp = [0] * (k + 1)
         dp[0] = 1
 
-        # open[j] = ways where j-th segment is currently open
+        
         open_seg = [0] * (k + 1)
 
         for _ in range(n - 1):
             for j in range(k, 0, -1):
-                # Start a new segment
+                
                 open_seg[j] = (
                     open_seg[j] + dp[j - 1]
                 ) % MOD
 
-                # Close the current segment
+                 
                 dp[j] = (
                     dp[j] + open_seg[j]
                 ) % MOD
